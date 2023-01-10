556c4d7198c0e92da6db04b7607ed54a3b2544cb
“NAME” <danronkam@gmail.com>
2023-01-09 21:41:42 -0800

    css updates

--- src/components/Tldr/index.js
+++ src/components/Tldr/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Tldr.css"
 
 
 const TLDR = () => {
@@ -7,9 +8,9 @@ const TLDR = () => {
         <>
         <div className="tldr-container">
             <div className="text-container">
-                <p className="tldr-text">Hi I'm Dan. I'm a Full Stack Engineer with expierence building client & server-side web application using Javascript.</p>
-                <p className="tldr-text">I mainly work within React, but I'm always expanding my tools and technologies. I'm currently working on a TypeScript project, but I also have frontend expierence with Next.js, JavaScript, and SEO Optimization.  </p>
-                <p className="tldr-text">On the Backend I have expierence with Ruby on Rails, Node js, MongoDB, AWS S3/IAM, and SQL based CMS like WordPress. </p>
+                <p className="tldr-text">Hi I'm <b id="name">Dan</b>. I'm a Full Stack Engineer with expierence building client & server-side web application using Javascript.</p>
+                <p className="tldr-text">I mainly work within <b>React</b>, but I'm always expanding my tools and technologies. I'm currently working on a <b>TypeScript</b> project, but I also have frontend expierence with <b>Next.js</b>, <b>JavaScript</b>, and <b>SEO Optimization</b>.  </p>
+                <p className="tldr-text">On the Backend I have expierence with <b>Ruby on Rails</b>, <b>Node js</b>, MongoDB, AWS S3/IAM, and SQL based CMS like WordPress. </p>
                 <p className="tldr-text">Previously I worked in Publishing and have expierence in content production, copy editing, and the Adobe Suite.</p>
             </div>
             <div className="projects-container">
