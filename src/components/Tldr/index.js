b87c73f044ba5ed0e7b1c9a00b7d1c451103de91
“NAME” <danronkam@gmail.com>
2023-01-15 21:15:51 -0800

    g

--- src/components/Tldr/index.js
+++ src/components/Tldr/index.js
@@ -23,7 +23,7 @@ const TLDR = () => {
             img: "https://derailed-seed.s3.us-west-1.amazonaws.com/dkfm_pc_gif.gif",
             alt: "dk-fm gif",
             tags: ["JavaScript", "HTML", "AWS", "CSS"],
-            bio: ">A Javascript music player that selects a list of music and accompanying visuals based on the users mood and preferences",
+            bio: "A Javascript music player that selects a list of music and accompanying visuals based on the users mood and preferences",
             live: "https://doafavorfor.me/DKFM247/",
             code: "https://github.com/danronkam/DKFM247"
         },
@@ -33,7 +33,7 @@ const TLDR = () => {
             img: "https://derailed-seed.s3.us-west-1.amazonaws.com/heard_pc_gif.gif",
             alt: "heard gif",
             tags: ["Rails", "React", "Redux", "Ruby", "JavaScript", "HTML", "AWS", "CSS"],
-            bio: "A fullstack Grailed clone where users can search through filtered listings, review other users profiles, create their own listings, and review products",
+            bio: "A MERN stack app where users can share their secrets anonymously, hear other confessions, and chat with other people in similar situations",
             live: "https://heard--app.herokuapp.com/home",
             code: "https://github.com/danronkam/Heard"
         },
