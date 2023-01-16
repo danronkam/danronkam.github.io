68bbcf0ce8dce54a93ac781d018c7c4be39d97ab
“NAME” <danronkam@gmail.com>
2023-01-15 16:32:06 -0800

    frames working

--- src/components/ProjectIndexItem/index.js
+++ src/components/ProjectIndexItem/index.js
@@ -16,33 +16,37 @@ const ProjectIndexItem = (project) => {
 
     return(
         <>
-
-        <div class="pj-window">
-            <div class="pj-header">
-                <div className="header-left">
-                    <p>{title}</p>
+        <div className="pj-container">
+            <div className="pj-window">
+                <div className="pj-header">
+                    <div className="header-left">
+                        <p>{title}</p>
+                    </div>
+                    <div className="header-right">
+                        <button className="icons">?</button>
+                        <button className="icons">X</button>
+                    </div>
                 </div>
-                <div className="header-right">
-                    <button className="icons">?</button>
-                    <button className="icons">X</button>
-                </div>
-            </div>
-            <div className="nav-tag">
-                {tags.map(tag => {
-                        return <button disabled  className="tag"> {tag} </button>
-                })}
-            </div>
-
-            
-            <div className="pj-body">
-                <img src={imgSrc} alt={altTxt} className="comp-gifs right-img"/>
-                <p className="pj-description">{bio}                </p>
-                <div className="links-container">
-                    <Link to={{ pathname: {codeSrc} }} target="_blank" className="pj-links">Code</Link> <Link to={{ pathname: {liveSrc} }} target="_blank" className="pj-links">Demo</Link>
+                <div className="nav-tag">
+                    {tags.map(tag => {
+                            return <button disabled  className="tag"> {tag} </button>
+                    })}
                 </div>
 
+                
+                <div className="pj-body">
+                    <img src={imgSrc} alt={altTxt} className="comp-gifs right-img"/>
+                    <p className="pj-description">{bio}                </p>
+                    <div className="links-container">
+                        <Link to={{ pathname: {codeSrc} }} target="_blank" className="pj-links">Code</Link> <Link to={{ pathname: {liveSrc} }} target="_blank" className="pj-links">Demo</Link>
+                    </div>
+
+                </div>
             </div>
+
         </div>
+
+       
 
         
         </>
