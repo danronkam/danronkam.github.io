bc01339d726c3ed511767f797a6d7ce699741990
“NAME” <danronkam@gmail.com>
2023-01-15 21:08:53 -0800

    cleaned up

--- src/App.js
+++ src/App.js
@@ -3,40 +3,16 @@ import { Switch, Route } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 
 import TLDR from "./components/Tldr";
-import AboutPage from "./components/Aboutpage";
-import ContactPage from "./components/ContactPage";
-import ProjectPage from "./components/ProjectPage";
-import MainPage from "./components/MainPage";
+
 
 function App() {
   return (
     <>
     <div className="page-container">
       <Switch>
-        <Route exact path="/tldr">
-          <TLDR/>
-        </Route>
-        <Route exact path="/about">
-          <AboutPage/>
-        </Route>
-        <Route exact path="/contact">
-          <ContactPage/>
-        </Route>
-        <Route exact path="/projects">
-          <ProjectPage />
-        </Route>
-        <Route exact path="/projects/heard">
-
-        </Route>
-        <Route exact path="/projects/derailed">
-
-        </Route>
-        <Route exact path="/projects/dkfm">
-          
-        </Route>
 
         <Route exact path="/">
-          <MainPage />
+          <TLDR />
         </Route>
 
         <Redirect to='/' />
