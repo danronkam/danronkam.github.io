2819a4f5f5165187e3cd59ef82080b4b255118cc
“NAME” <danronkam@gmail.com>
2023-04-10 19:14:54 -0700

    warnings fix

--- src/components/ProjectIndexItem/index.js
+++ src/components/ProjectIndexItem/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./ProjectIndexItem.css"
 
 
@@ -39,7 +38,7 @@ const ProjectIndexItem = (project) => {
                     <img src={imgSrc} alt={altTxt} className="comp-gifs right-img"/>
                     <p className="pj-description">{bio}                </p>
                     <div className="links-container">
-                        <a href={codeSrc} target="_blank" className="icons pj-links">Code</a> <a href={liveSrc} target="_blank" className="icons pj-links">Demo</a>
+                        <a href={codeSrc} target="_blank" rel="noreferrer" className="icons pj-links">Code</a> <a href={liveSrc} className="icons pj-links">Demo</a>
                     </div>
 
                 </div>
