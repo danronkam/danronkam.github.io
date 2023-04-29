4ca696513e2ceb486909ff975c5286113d419698
“NAME” <danronkam@gmail.com>
2023-04-28 20:37:55 -0700

    new bio uploaded

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -6,6 +6,19 @@ const About = () => {
     return(
         <>
         <h1>I am an about page</h1>
+        <p>
+        Meet Dan, a talented full-stack software engineer with a passion for creating dynamic and robust applications using TypeScript. With over five years of experience in the industry, Dan has honed his skills in both front-end and back-end development, enabling him to deliver end-to-end solutions for complex software projects.
+
+Dan's expertise in TypeScript is unparalleled, and he uses it as his primary language for building applications. He has a deep understanding of TypeScript's features and benefits, including its static typing, object-oriented programming, and enhanced code safety, making it his go-to language for creating high-performance, scalable applications.
+
+Dan is well-versed in front-end development technologies like React, Angular, and Vue.js, and he has developed numerous applications using these frameworks. He is proficient in using CSS preprocessors like Sass and LESS and has experience in developing responsive and accessible user interfaces.
+
+On the back-end, Dan is comfortable working with Node.js, Express, and Nest.js, and has experience in designing RESTful APIs, integrating with databases, and implementing authentication and authorization schemes. He is also proficient in using testing frameworks like Jest and Mocha to ensure that his applications are reliable and robust.
+
+Throughout his career, Dan has collaborated with cross-functional teams, including product managers, designers, and other developers, to deliver quality software that meets business requirements. He is passionate about staying up-to-date with the latest technology trends and applying them to his work to create innovative solutions.
+
+Dan's strong problem-solving skills, attention to detail, and ability to learn quickly make him an asset to any team. If you're looking for a full-stack software engineer who can create high-quality TypeScript applications, Dan is your go-to guy
+        </p>
         </>
     )
 }
