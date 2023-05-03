902694aedb8f32f86fad96ed9874e470f6553762
“NAME” <danronkam@gmail.com>
2023-05-03 08:33:03 -0700

    css progress

--- frontend/src/components/Blog/index.js
+++ frontend/src/components/Blog/index.js
@@ -4,13 +4,21 @@ import "./Blog.css"
 
 
 function Blog({blogPosts}) {
+
+    if(!blogPosts) {
+        blogPosts = [];
+    }
+
     return(
         <>
         <div className="blog-index-container">
+            <div className="toc">
+            
+            </div>
 
-            <div className="post">
+            <div className="post-containter">
                 {blogPosts.map(post => {
-                return <BlogIndexItem key={post.id} post={post} />
+                    return <BlogIndexItem key={post.id} post={post} />
                 })}
 
             </div>
