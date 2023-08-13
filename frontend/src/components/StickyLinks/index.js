92bdc701ef64690caf7f272c3d0f68a7df9d8068
danronkam <danronkam@gmail.com>
2023-08-12 20:47:14 -0700

    set up

--- frontend/src/components/StickyLinks/index.js
+++ frontend/src/components/StickyLinks/index.js
@@ -4,8 +4,13 @@ const StickyLinks = () => {
     return(
         <>
             <div className="sticky-container">
-
-                
+                <div className="sticky-top">
+                    <button>Github</button>
+                    <button>Live</button>
+                </div>
+                <div className="sticky-bottom">
+                    <button>Contact</button>
+                </div>
             </div>
         
         </>
