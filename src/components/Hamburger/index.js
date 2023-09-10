e3fa8b11f5ca54cb277e55b69b62761e32c30f28
danronkam <danronkam@gmail.com>
2023-09-09 23:23:43 -0700

    js func

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -10,6 +10,16 @@ const HamburgerMenu = () => {
     console.log('uh oh')
   };
 
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
   return (
     <>
       <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
