1beac60507b693b7eeae90f2f5ddf1a810a121b1
danronkam <danronkam@gmail.com>
2024-01-02 16:14:58 -0800

    ecom highlight

--- src/components/Highlights/index.js
+++ src/components/Highlights/index.js
@@ -6,6 +6,15 @@ import "./Highlights.css"
 const Highlights = () => {
     const highlightsData = [
         {
+            id: 3,
+            title: "Ecommerce Developer",
+            type: "Work", 
+            date: "2023",
+            blurb: "Elevate your business online with custom tailored solutions to fit your needs. With proven results across platofrms, Cargo, Shopify, WooCommerce, check out some of my previous work and see how I can help you grow online",
+            link: "/ecom",
+
+        },
+        {
             id: 1,
             title: "Derailed",
             type: "Project", 
@@ -20,15 +29,6 @@ const Highlights = () => {
             date: "2022",
             blurb: "Heard is a full stack MERN project that introduces an intriguing web application where users can share anonymous confessionals that are securely stored in a database and sent to another user for a limited viewing period before being permanently deleted. Additionally, users can create anonymous chat rooms to engage in private conversations. Developed within a week, Heard harnesses the power of MongoDB for efficient data storage, Express.js for building a robust backend, React for creating an interactive user interface, and Next.js for server-side rendering. This combination of technologies delivers a seamless and secure platform for anonymous confessions and private chats.",
             link: "/project/:heard",
-
-        },
-        {
-            id: 3,
-            title: "DKFM",
-            type: "Project", 
-            date: "2022",
-            blurb: "DKFM is a captivating web application that seamlessly blends music and visuals to create an immersive experience tailored to the user's mood. Serving as both a music player and visualizer, DKFM curates a playlist that dynamically matches the ambiance desired by the user. Crafted using JavaScript, HTML, and CSS, this application provides a seamless and intuitive interface for users to explore an array of audio-visual combinations, enhancing their listening experience with synchronized visuals that perfectly complement the music.",
-            link: "/project/:dkfm",
 
         }
     ]
