3492458d84475a38925be5f1b9b0cce1239e55fc
danronkam <danronkam@gmail.com>
2024-01-18 10:04:16 -0800

    test

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -6,23 +6,50 @@ const Services = () => {
     return(
         <>
             <div className="content-container">
-                <div className="services-about">
+                <div className="services-about-container">
+                    <p>
+                        s
+                    </p>
+                </div>
+
+                <div className="services-list-container">
+                    <ul className="service-list">
+                        <li className="service">
+                            Full Stack Web Development  
+                        </li>
+                        <li className="service">
+                            Tech Consulting
+                        </li>
+                        <li className="service">
+                            Brand Strategy 
+                        </li>
+                        <li className="service">
+                            Data Analysis
+                        </li>
+                        <li className="service">
+                            Multichannel Selling
+                        </li>
+                        <li className="service">
+                            Brand Strategy 
+                        </li>
+                        <li className="service">
+                            Product Development 
+                        </li>
+                        <li className="service">
+                            Copywriting 
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="services-clients-container">
 
                 </div>
 
-                <div className="services-list">
-
-                </div>
-
-                <div className="services-clients">
-
-                </div>
-
-                <div className="services-contact">
+                <div className="services-contact-container">
 
                 </div>
                 
-                <div className="services-press">
+                <div className="services-press-container">
                     
                 </div>
 
