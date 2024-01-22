de0f7e3725e9e1cbd25333c36b8f56889d5d29aa
danronkam <danronkam@gmail.com>
2024-01-22 07:31:46 -0800

    starting

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -6,53 +6,76 @@ const Services = () => {
     return(
         <>
             <div className="content-container">
-                <div className="services-about-container">
-                    <p>
-                        s
+                <div className="content-left">
+                    <h1 className="content-title">Web Development Services </h1>
+                    <p className="content-description">
+                        Brand consultancy and web development working across scale, strategy, creative direction and technical research. 
                     </p>
-                </div>
+                    <div className="tag-wrapper">
+                        <div className="content-tags flex-child">
+                            <div className="label">Type</div>
+                            <div className="tags">For Hire</div>
+                        </div>
+                        <div className="content-tags flex-child">
+                            <div className="label">Updated</div>
+                            <div className="tags">Jan 2024</div>
+                        </div>
+                    </div>
 
-                <div className="services-list-container">
-                    <ul className="service-list">
-                        <li className="service">
-                            Full Stack Web Development  
-                        </li>
-                        <li className="service">
-                            Tech Consulting
-                        </li>
-                        <li className="service">
-                            Brand Strategy 
-                        </li>
-                        <li className="service">
-                            Data Analysis
-                        </li>
-                        <li className="service">
-                            Multichannel Selling
-                        </li>
-                        <li className="service">
-                            Brand Strategy 
-                        </li>
-                        <li className="service">
-                            Product Development 
-                        </li>
-                        <li className="service">
-                            Copywriting 
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="services-clients-container">
-
-                </div>
-
-                <div className="services-contact-container">
-
-                </div>
-                
-                <div className="services-press-container">
+                    <hr />
                     
-                </div>
+                    <div className="content-body">
+                        <div className="introduction">
+                            <h2 className="content-heading">About</h2>
+                            <p className="">
+                                Helping busineeses to  
+                            </p>
+                        </div>
+                    </div>
 
+                    <div className="services-list-container">
+                        <ul className="service-list">
+                            <li className="service">
+                                Full Stack Web Development  
+                            </li>
+                            <li className="service">
+                                Tech Consulting
+                            </li>
+                            <li className="service">
+                                Brand Strategy 
+                            </li>
+                            <li className="service">
+                                Data Analysis
+                            </li>
+                            <li className="service">
+                                Multichannel Selling
+                            </li>
+                            <li className="service">
+                                Brand Strategy 
+                            </li>
+                            <li className="service">
+                                Product Development 
+                            </li>
+                            <li className="service">
+                                Copywriting 
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="services-clients-container">
+
+                    </div>
+
+                    <div className="services-contact-container">
+
+                    </div>
+                    
+                    <div className="services-press-container">
+                        
+                    </div>
+
+
+                </div>`
 
             </div>
                 
