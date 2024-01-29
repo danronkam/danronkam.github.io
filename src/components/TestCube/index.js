d3ac540ae371d985950e0f340b386f55d4810a01
danronkam <danronkam@gmail.com>
2024-01-28 16:25:31 -0800

    if youre reading this, it is a cry for help

--- src/components/TestCube/index.js
+++ src/components/TestCube/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Cube from "../CubeComponent"
+import "./LogoCube.css"
 
 class TestCube extends React.Component {
     render() {
@@ -25,6 +26,8 @@ class TestCube extends React.Component {
                         <img alt="Dan Kam" style={{width: '400px', height: '400px'}} src="https://derailed-seed.s3.us-west-1.amazonaws.com/kamcube_11.png"></img>
                     </div>
                 </Cube>
+                <h1>fuck</h1>
+
             </div>
         );
     }
