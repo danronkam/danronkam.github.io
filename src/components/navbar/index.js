194d96818c4926f10f2a7e638155b1c851a18ca7
danronkam <danronkam@gmail.com>
2024-02-15 10:49:06 -0800

    no logos

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -40,22 +40,7 @@ function Navbar() {
                 <i className="fas fa-user" /> Projects
               </button>
             </li>
-  
-            <li className='nav-link '> 
-            <button className='nav-button' value={'github.com'} onClick={()=> window.open("https://www.github.com/danronkam", "_blank")} type="button">
-                <i className="icon fab fa-github"/>
-            </button>
-            </li>
-            <li className='nav-link '> 
-            <button className='nav-button' value={'/linkedin'} onClick={()=> window.open("https://www.linkedin.com/in/danronkam", "_blank")} >
-                <i className="icon fab fa-linkedin"/>
-              </button>
-            </li>
-            <li className='nav-link '> 
-            <button className='nav-button' value={'/substack'} onClick={()=> window.open("https://designdk.substack.com", "_blank")} >
-                <img src='https://derailed-seed.s3.us-west-1.amazonaws.com/ss_purple.png' alt='substack'/>
-              </button>
-            </li>
+
             <li className='nav-link '> 
             <button className='nav-button' value={'/contact'} onClick={(e) => handleClick(e.target.value)}>
               <i className="fa fa-pen-fancy"/> Contact
