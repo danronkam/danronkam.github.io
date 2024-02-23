5fdd81785012a5909e5156ae274764a282e178dd
danronkam <danronkam@gmail.com>
2024-02-22 22:00:03 -0800

    item background img passed as props

--- src/components/CardIndexItem/index.js
+++ src/components/CardIndexItem/index.js
@@ -1,10 +1,17 @@
 const CardIndexItem = ({ item }) => {
+
+    const cardStyle = {
+        backgroundImage: `url(${item.backgroundImage})`,
+    };
+
     return (
+        
       <div className="card">
         <h3>{item.title}</h3>
         <p>{item.description}</p>
       </div>
     );
   };
+  
 
   export default CardIndexItem
