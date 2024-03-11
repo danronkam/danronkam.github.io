a30b0fe6775b3d34867cb2d79010f6791e5db03e
danronkam <danronkam@gmail.com>
2024-03-10 23:05:34 -0700

    click to copy not working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,48 +7,50 @@ const Contact = () => {
     useLayoutEffect(() => {
         window.scrollTo(0,0);
     });
-    const links = document.querySelectorAll('.copy-click');
+    // const links = document.querySelectorAll('.copy-click');
 
-    const cls = {
-        copied: 'is-copied',
-        hover: 'is-hovered'
-    }
 
-    const copyToClipboard = str => {
-        const el = document.createElement('input');
 
-        str.dataset.copyString ? el.value = str.dataset.copyString : el.value = str.text;
-        el.setAttribute('readonly', '');
-        el.style.position = 'absolute';
-        el.style.opacity = 0;
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    }
+    // const cls = {
+    //     copied: 'is-copied',
+    //     hover: 'is-hovered'
+    // }
 
-    const clickInteraction = (e) => {
-        e.preventDefault();
-        copyToClipboard(e.target);
-        e.target.classList.add(cls.copied);
-        setTimeout(() => e.target.classList.remove(cls.copied), 1000);
-        setTimeout(() => e.target.classList.remove(cls.hover), 700);  
-    }
+    // const copyToClipboard = str => {
+    //     const el = document.createElement('input');
 
-    Array.from(links).forEach(link => {
-        link.addEventListener('click', e => clickInteraction(e));
+    //     str.dataset.copyString ? el.value = str.dataset.copyString : el.value = str.text;
+    //     el.setAttribute('readonly', '');
+    //     el.style.position = 'absolute';
+    //     el.style.opacity = 0;
+    //     document.body.appendChild(el);
+    //     el.select();
+    //     document.execCommand('copy');
+    //     document.body.removeChild(el);
+    // }
+
+    // const clickInteraction = (e) => {
+    //     e.preventDefault();
+    //     copyToClipboard(e.target);
+    //     e.target.classList.add(cls.copied);
+    //     setTimeout(() => e.target.classList.remove(cls.copied), 1000);
+    //     setTimeout(() => e.target.classList.remove(cls.hover), 700);  
+    // }
+
+    // Array.from(links).forEach(link => {
+    //     link.addEventListener('click', e => clickInteraction(e));
         
-        link.addEventListener('keypress', e => {
-            if (e.keyCode === 13) clickInteraction(e);
-        });
+    //     link.addEventListener('keypress', e => {
+    //         if (e.keyCode === 13) clickInteraction(e);
+    //     });
 
-        link.addEventListener('mouseover', e => e.target.classList.add(cls.hover));
-        link.addEventListener('mouseleave', e => {
-            if (!e.target.classList.contains(cls.copied)) {
-            e.target.classList.remove(cls.hover); 
-            }
-        });
-    });
+    //     link.addEventListener('mouseover', e => e.target.classList.add(cls.hover));
+    //     link.addEventListener('mouseleave', e => {
+    //         if (!e.target.classList.contains(cls.copied)) {
+    //         e.target.classList.remove(cls.hover); 
+    //         }
+    //     });
+    // });
 
 
 
